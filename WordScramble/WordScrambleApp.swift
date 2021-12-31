25673cc82eed58cda0cf6c69c8da852d326699ff
Leopold Lemmermann <leopold@lemmermann.com>
2021-12-31 02:22:27 +0100

    Leaderboard entries are now saved to iCloud and shared across a users devices. The entries can no longer be removed. Global leaderboard will hopefully be up and running soon.

--- WordScramble/WordScrambleApp.swift
+++ WordScramble/WordScrambleApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct WordScrambleApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onChange(of: scenePhase) { _ in persistenceController.saveContext() }
         }
     }
 }
